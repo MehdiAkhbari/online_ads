8418da2d9b0f4879603fe74b46731b9937f5b933
Akhbari <ma59396@my.utexas.edu>
2023-12-14 10:16:25 -0600

    Updated codes! Now estimation is working!

--- scripts/estimation.py
+++ scripts/estimation.py
@@ -3,26 +3,64 @@ from utils import *
 
 
 # read data
-data = pd.read_stata("..\\data\\Estimation Data by Subject - Last Two Days Binary.dta")
+file_name = "Estimation Data by Subject - Last Two Days Binary - split 1.dta"
+file_dir = "..\\data\\"
+file_dir_name = file_dir + file_name
+data = pd.read_stata(file_dir_name)
 # prepare data for estmation
 prepare_data(data, base_ad=50, max_ad=100)
 # extract advertiser ranks
 ranks_list = extract_ranks(data)
-
-for rank in [99, 100]:
+# drop rank 0 (based ad) from the list, for the base ad we don't calculate trwatment effect
+ranks_list.pop(0)
+for rank in ranks_list:
     start_time_1 = time.perf_counter()
     print(f"Estimating for Rank {rank}")
     # select the subset of data for current estimation:
     df = data[(data['advertiser_rank'] == 0) | (data['advertiser_rank'] == rank)].reset_index(drop=True).copy()
     (X, Y, T) = define_xyt(df)
+    # find best parameters for the m model
+    best_params = m_model_best_estimator(X, Y)
     # estimate the casual forest model
-    estimated_forest = causal_forest_estimate(X, Y, T, cf_param_grid)
+    # define the causal forest model
+    cf = CausalForestDML(
+                            model_y=RandomForestRegressor(**best_params),
+                            model_t=propensity_model,
+                            discrete_treatment='True',
+                            criterion='het',
+                            n_jobs=n_jobs,
+                            n_estimators=100,
+                            min_samples_split=1000,
+                            max_depth=20,
+                            max_samples=0.01,
+                            random_state=42,
+                            verbose=0   
+        )
+    
+ # tune the model:
+    start_time = time.perf_counter()
+
+    tune_params = cf.tune(
+                Y=Y,
+                T=T,
+                X=X,
+                params=cf_param_grid)
+    
+    finish_time = time.perf_counter()
+
+    print(f"finished tuning the model in {finish_time - start_time} seconds")
+
+    # fit the model using tuned parameters:
+    start_time = time.perf_counter()
+    
+    cf.fit(Y=Y, T=T, X=X, inference="blb", cache_values=True)
+    
+    finish_time = time.perf_counter()
+    print(f"finished fitting the model in {finish_time - start_time} seconds")
+
     # save the model
-    file_name = f"..\\results\\CF - Rank {rank}.pkl"
-    joblib.dump(estimated_forest, file_name)
-
-
-
-
-
+    file_name = f"..\\results\\split 1\\CF - Rank {rank}.pkl"
+    joblib.dump(cf, file_name)
+    finish_time_1 = time.perf_counter()
+    print(f"finished rank {rank} in {finish_time_1 - start_time_1} seconds")
 
--- scripts/utils.py
+++ scripts/utils.py
@@ -13,7 +13,117 @@ from econml.sklearn_extensions.model_selection import GridSearchCVList
 import time
 import joblib
 
-print("Done!")
+# set number of cores to use
+n_jobs = 30
+
+def read_data(data_set_name):
+    file_name = f"..\\data\\{data_set_name}"
+    data = pd.read_stata(file_name)
+    return data
+
+
+def prepare_data(data, base_ad=50, max_ad=100):
+    # Set base treatment advertiser_rank =0
+    data.loc[data['advertiser_rank'] == base_ad, 'advertiser_rank'] = 0
+    # set advertiser_rank = 101 for 100+ ranked advertisers
+    data.loc[data['advertiser_rank'] > max_ad, 'advertiser_rank'] = (max_ad + 1)
+
+def extract_ranks(data):
+    # extract the list of available advertiser_ranks
+    ranks_list = data['advertiser_rank'].value_counts().sort_index().index.tolist()
+    return ranks_list
+
+
+class PropensityModel(BaseEstimator):
+    def __init__(self):
+        self.lr = LogisticRegression(max_iter=2000)
+
+    def predict_proba(self, X, X_indices=slice(-27,-1)):
+        return self.lr.predict_proba(X[:,X_indices])
+
+
+    
+    # X_indices are the ones that are used for the estimation of the propensity score
+    def fit(self, X, y, X_indices=slice(-27,-1)):
+        self.lr.fit(X[:,X_indices], y)
+        return self
+
+ # Instantiate propensity_model from the PropensityModel class
+propensity_model = PropensityModel()
+
+
+# Define the hyperparameters to search over
+param_grid = {
+    'n_estimators': [50, 100, 200],
+    'max_depth': [10, 20, 30],
+    'min_samples_split': [1000 , 2000, 3000, 5000]
+}
+
+# Define the hyperparameters to search over
+cf_param_grid = {
+    'n_estimators': [100, 200, 300],
+    'max_depth': [10, 20, 30],
+    'min_samples_split': [1000 , 2000, 3000, 5000],
+    'max_samples': [0.1, 0.2, 0.3]
+}
+
+
+
+
+def define_xyt(data):
+    # define X
+    X = data.drop(['publisher_subject', 'advertiser_rank', 'is_clicked', 'event_no', 'prop'], axis=1)
+    
+    # define T
+    T = data['advertiser_rank']
+    
+    # define Y
+    Y = data['is_clicked']
+    return X, Y, T
+
+
+def m_model_best_estimator(X, Y, n_jobs=30):
+    start_time = time.perf_counter()
+    m_model = RandomForestRegressor(verbose=0, n_jobs=n_jobs)
+     
+    # Perform grid search cross-validation
+    grid_search = GridSearchCV(estimator=m_model, param_grid=param_grid, cv=5)
+    grid_search.fit(X, Y)
+    
+    best_params = grid_search.best_params_
+    best_estimator = grid_search.best_estimator_
+    finish_time = time.perf_counter()
+    print(f"finished tuning the M model in {finish_time - start_time} seconds")
+    return best_params
+
+
+    
+
+# def causal_forest_estimate(X, Y, T, cf_param_grid):
+#     # tune the model:
+#     start_time = time.perf_counter()
+
+#     cf.tune(
+#                 Y=Y,
+#                 T=T,
+#                 X=X,
+#                 params=cf_param_grid)
+    
+#     finish_time = time.perf_counter()
+#     print(f"finished tuning the model in {finish_time - start_time} seconds")
+
+#     # fit the model using tuned parameters:
+#     start_time = time.perf_counter()
+    
+#     cf.fit(Y=Y, T=T, X=X, inference="blb", cache_values=True)
+    
+#     finish_time = time.perf_counter()
+#     print(f"finished fitting the model in {finish_time - start_time} seconds")
+#     return cf
+
+
+
+    
 
 
 
